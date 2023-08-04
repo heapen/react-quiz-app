1531c44c14718c11deb6bf1b86688e163ad6b543
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-04 13:39:06 +0300

    API Added

--- src/pages/introduce/introduce.jsx
+++ src/pages/introduce/introduce.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './introduce.css'
+import Dropdown from "../../companents/dropdown/dropdown";
+import { useNavigate } from "react-router-dom";
 const Introduce = () => {
+    const difficulty = ["easy", "medium", "hard"]
+    const [difficultyChange, setDifficultyChange] = useState('')
+    const navigate = useNavigate()
+    const TOTAL_QUESTIONS = 10
+    const startQuiz = () => {
+        if(difficultyChange){
+            navigate(`/quiz/${difficultyChange}/${TOTAL_QUESTIONS}`)
+        }
+    }
     return (
-        <div className="introduce">Introduce</div>
+        <div className="introduce">
+            <div className="introduce-container">
+                <img  src="https://www.youthdownloads.com//triviamaker/wp-content/themes/stratusx-child/template/upload/UraXLTV8vcP72YAUTkWoerz5nZB2/2021052716221412191886/388logo.png" alt="" />
+                <Dropdown data= {difficulty} setDifficultyChange={setDifficultyChange}/>
+                <div onClick={startQuiz} className="introduce-btn">Start Quiz</div>
+            </div>
+           
+        </div>
     )
 }
 
--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './quiz.css'
+import * as api from '../../api/api'
+import { useParams } from "react-router-dom";
 
 const Quiz = () => {
+    const {difficulty, amount} = useParams()
+    const [questionsData, setQuestionsData] = useState([]);
+
+    
+    useEffect(() => {
+     const getData = async() => {
+        const data = await api.fetchQuizData(difficulty, amount)
+        setQuestionsData(data)
+    }
+     getData();
+     }, [])
+     console.log(questionsData)
+
     return (
-        <div className="quiz">Quiz</div>
+        <div className="quiz">
+            {difficulty} - {amount}
+        </div>
     )
 }
 
